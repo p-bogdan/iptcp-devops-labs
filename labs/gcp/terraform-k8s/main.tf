7a8925af8ed25fe028a6bcba651db92cfb0c1b39
Petro Bogdan <you@example.com>
2021-01-25 16:24:55 +0200

    Updating lab configs

--- labs/gcp/terraform-k8s/main.tf
+++ labs/gcp/terraform-k8s/main.tf
@@ -4,11 +4,15 @@ module "compute_network" {
    region              = "europe-west1"
    network_name        = "k8s-network"
    ipv4_range_backends = "10.132.5.0/24"
+   source_ranges       = ["0.0.0.0/0", "130.211.0.0/22", "35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
+   fw_ports            = ["80", "8080", "443", "3306", "22"]
  }
 
 module "gke" {
   source  = "git@github.com:ciscoios/iptcp-gcp-reusable-modules.git//modules/gcp/gke"
   project = "lab-project-282605"
   zone = "europe-west1-b"
+  cluster_name = "k8s-lab"
+  node_count = "3"
 }
 
