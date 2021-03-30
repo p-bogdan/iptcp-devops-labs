60297ae29973af38a9d6dd738aa5f1b8a408326b
Petro Bogdan <you@example.com>
2021-03-30 11:49:35 +0300

    Updated google provider version

--- labs/gcp/terraform-k8s-jenkins-auth/provider.tf
+++ labs/gcp/terraform-k8s-jenkins-auth/provider.tf
@@ -32,7 +32,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.60.0"
+      version = "~> 3.62.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
