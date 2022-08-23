d609797fd399be7ca720d99281209e4fb8365f0e
Petro Bogdan <you@example.com>
2022-08-23 23:46:47 +0300

    Adding encryption

--- labs/aws/packer/main.tf
+++ labs/aws/packer/main.tf
@@ -3,6 +3,17 @@ resource "aws_instance" "web" {
   ami           = "ami-0f11fc02196137199"
   instance_type = "t2.micro"
 
+root_block_device {
+      encrypted = true
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = false
+    encrypted = true
+  }
   tags = {
     Name = "Using Packer AMI"
   }
