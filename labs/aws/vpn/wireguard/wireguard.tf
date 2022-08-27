7bf247db6f2934cc312e7e754541058ba5826b44
Petro Bogdan <you@example.com>
2022-08-27 18:56:05 +0300

    Updating aws infra tags

--- labs/aws/vpn/wireguard/wireguard.tf
+++ labs/aws/vpn/wireguard/wireguard.tf
@@ -156,12 +156,6 @@ resource "aws_instance" "bastion" {
 		#!/bin/bash
     yum install -y tree 
     amazon-linux-extras install -y epel
-    # set repo url and file #
-    #export rwfile="/etc/yum.repos.d/wireguard.repo"
-    #export rwurl="https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo"
-    # Download it
-    #wget --output-document="$rwfile" "$rwurl"
-    #yum clean all && yum install -y wireguard-dkms wireguard-tools
 	EOF
   root_block_device {
     #device_name           = "/dev/xvda"
@@ -197,6 +191,7 @@ key_name               = aws_key_pair.kp.key_name
     Name  = "bastion-01"
     Stack = "dev"
     Owner = "Petro Bogdan"
+    ansible_hostgroup = "wireguard"
   }
 }
 
