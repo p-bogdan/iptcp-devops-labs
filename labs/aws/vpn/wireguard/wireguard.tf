e02d97ade150df787188bb572d2ade8d5a1dd9f9
Petro Bogdan <you@example.com>
2022-08-30 21:48:34 +0300

    Testing github ips whitelisting

--- labs/aws/vpn/wireguard/wireguard.tf
+++ labs/aws/vpn/wireguard/wireguard.tf
@@ -210,7 +210,7 @@ resource "aws_security_group" "bastion-01-sg" {
     to_port     = 22
     protocol    = "tcp"
     #cidr_blocks = ["92.60.179.185/32"]
-    cidr_blocks = ["92.60.179.185/32", "${chomp(data.http.myip.response_body)}/32"]
+    cidr_blocks = ["92.60.179.185/32", "52.168.30.116/32", "13.67.131.126/32", "20.66.83.71/32"]
     #cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
 
   }
