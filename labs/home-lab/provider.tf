003d3236fcd9650d4f082e2601acfc90618a0191
Petro Bogdan <iptcp@local>
2023-06-29 08:34:46 +0300

    Updating lab files

--- labs/home-lab/provider.tf
+++ labs/home-lab/provider.tf
@@ -1,3 +1,22 @@
 provider "kubernetes" {
 config_path = "~/.kube/config"
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      #version = "~> 2.6.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      #version = "~> 2.4.1"
+    }
+  }
+}
