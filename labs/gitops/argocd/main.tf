8f7dad33fb1c0dd4b2717c91cd0b19bbaa70de07
Petro Bogdan <iptcp@local>
2023-07-13 22:11:06 +0300

    Updating terraform version and argocd setup

--- labs/gitops/argocd/main.tf
+++ labs/gitops/argocd/main.tf
@@ -3,16 +3,20 @@ module "compute_network" {
   project             = "ci-cd-387713"
   region              = "us-east1"
   network_name        = "k8s-network"
-  ipv4_range_backends = "10.132.5.0/24"
+  ipv4_range_backends = "10.0.0.0/16"
   source_ranges       = ["0.0.0.0/0"]
-  fw_ports            = ["80", "8080", "443", "22"]
+  #fw_ports            = ["80", "8080", "443", "22", "1024-65535"]
+  fw_ports            = ["80", "8080", "443", "22", "1024-65535"]
 }
 
 module "gke" {
+  depends_on               = [module.compute_network]
   source                   = "git@github.com:p-bogdan/iptcp-gcp-reusable-modules.git//modules/gcp/gke"
   project                  = "ci-cd-387713"
   zone                     = "us-east1-b"
   cluster_name             = "k8s-lab"
+  network                  = "k8s-network"
+  subnetwork               = "gcp-lab-backend-subnet"
   node_count               = "2"
   machine_type             = "n1-standard-1"
   disk_size_gb             = 20
@@ -35,4 +39,8 @@ resource "helm_release" "argocd" {
   wait = true
   wait_for_jobs = true
   create_namespace = true
+  set {
+    name  = "server.service.type"
+    value = "NodePort"
+  }
 }
