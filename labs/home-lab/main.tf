2d9c1586786728fef62295051efa3aaccef3a788
Petro Bogdan <iptcp@local>
2024-04-27 19:08:12 +0300

    Adding argocd setup

--- labs/home-lab/main.tf
+++ labs/home-lab/main.tf
@@ -1,12 +1,12 @@
-#module "falco" {
-#source = "./falco"
-#}
-
-#module "linkerd" {
-#  source              = "./linkerd"
-#}
-
 module "argocd" {
 source = "./argocd"
+chart_version = "6.7.17"
+}
+
+# Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_dev_root]
+module "argocd_dev_root" {
+  source             = "./argocd/terraform_argocd_dev_root"
+  git_source_path    = "lab-dev/applications"
+  git_source_repoURL = "git@github.com:p-bogdan/argocd-charts.git"
 }
 
